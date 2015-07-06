aca559637e682432578d5c4f19c582163352b027
Alexander Razzhivin <razzhivinalexander@gmail.com>
2015-07-06 19:33:29 +0300

    Added custom prefix for course name in courses sub-grid. Removed useless file from tracking.

--- src/main/java/dev/university/connector/CoursesConnectorServlet.java
+++ src/main/java/dev/university/connector/CoursesConnectorServlet.java
@@ -1,5 +1,7 @@
 package dev.university.connector;
 
+import com.dhtmlx.connector.ConnectorBehavior;
+import com.dhtmlx.connector.DataItem;
 import com.dhtmlx.connector.GridConnector;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
  * Provides courses for the particular subject.
  */
 public class CoursesConnectorServlet extends BaseConnectorServlet {
+	private static final String COURSE_NAME_PREFIX = "Курс:";
+
 	@Override
 	protected void configure(HttpServletRequest req, HttpServletResponse res) {
 		try {
@@ -18,6 +22,10 @@ public class CoursesConnectorServlet extends BaseConnectorServlet {
 			final int subjectId = Integer.valueOf(req.getParameterMap().get("id")[0]);
 
 			final GridConnector gridConnector = makeGridConnector(req, res);
+
+			// Adding our custom render behaviour.
+			gridConnector.event.attach(new RenderBehaviour());
+
 			gridConnector.render_sql(
 					// I am aware of sql injections but I did not find the way how to use parameterized sql queries.
 					String.format("select * from courses where subjectId = %d", subjectId),
@@ -28,6 +36,22 @@ public class CoursesConnectorServlet extends BaseConnectorServlet {
 			);
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private class RenderBehaviour extends ConnectorBehavior
+	{
+		@Override
+		public void beforeRender(DataItem data) {
+			// Adding our predefined prefix.
+			String key = "courseName";
+			String courseName = data.get_value(key);
+			data.set_value(key, String.join(" ", new String[]{
+					COURSE_NAME_PREFIX,
+					courseName
+			}));
+
+			super.beforeRender(data);
 		}
 	}
 }
